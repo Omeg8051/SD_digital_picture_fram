dfd3d8f9a8262f10b3e0877b3a1fa44c7bba808e
omeg <east0987987@gmail.com>
2026-03-09 18:25:57 +0800

    Initial proposal of simulation hardware models.

---
+++ src/tb_model/lcd_mock.sv
@@ -0,0 +1,5 @@
+/*
+Read SPI and pin input and verify it against a 
+hard coded list and mark difference with $display
+system routune.
+*/
---
+++ src/tb_model/sd_mock.sv
@@ -0,0 +1,5 @@
+/*
+Read SPI and pin input and verify it against a 
+harcoded sd card read-only model and mark
+difference with $display system routune.
+*/
